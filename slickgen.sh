bd9287720876257289c1c29fda34c07fb0831ade
matan <dev.matan@gmail.com>
2015-02-14 04:37:02 +0200

    "codegen script - runs the slick codegen project and copies the result"

--- slickgen.sh
+++ slickgen.sh
@@ -1,35 +1,13 @@
 color='\e[1;34m'
 detail_color='\e[1;30m'
 NC='\e[0m' # No Color
-echo -e ${color}
-echo "running slick code generation..."
-echo -e ${detail_color}
+echo -e ${color}"starting slick code generation to auto-create model from database..." ${detail_color}
 cd ../slick-codegen
 sbt slickGenerate
 cd ../articlio
-echo -e ${color}
-echo "backing up current slick model..."
-echo -e ${detail_color}
+echo -e ${color}"backing up current model..." ${detail_color}
 mv app/models/Tables.scala app/models/TablesOld.scala
-echo -e ${color}
-echo "copying new generated slick model..."
-echo -e ${detail_color}
+echo -e ${color}"copying new generated slick model..." ${detail_color}
 cp ../slick-codegen/target/scala-2.11/src_managed/articlio/models/Tables.scala app/models/
-echo -e ${color}
-echo "if no errors - done"
-echo -e ${detail_color}
-
-
-
-echo "Trying to stage and commit all changes"
-echo -e ${detail_color}
-git status
-git add .
-git add -u
-git status
-echo "Trying to commit with comment \"$1\""
-echo -e ${color}
-git commit -am "\"$1\""
-echo
-git status
+echo -e ${color}"if no errors - done" ${detail_color}
 echo -e ${NC}
