9457ba06fb31426cd5d436a561cd3b69d13e0313
matan <dev.matan@gmail.com>
2015-03-17 23:37:49 +0200

    "declutter created data dep logging"

--- src/com/articlio/dataExecution/DataObjectExecution.scala
+++ src/com/articlio/dataExecution/DataObjectExecution.scala
@@ -37,13 +37,13 @@ trait DataExecution extends Connection {
       
       // maybe a bit ugly string composition, comprising nested formatting strings.
       s"${executionTree.accessOrError match {
-          case access:      Access      => "created Ok,"
-          case accessError: AccessError => accessError.errorDetail
-         }} ${children.isEmpty match { 
-               case true  => "had no dependencies"
-               case false => s"dependencies' details: ${children.map(child =>
-                            s"\ndependency ${child.data} - ${doSerialize(child)}")}"
-             }
+          case access:      Access      => "created Ok - "
+          case accessError: AccessError => accessError.errorDetail + " "
+         }}${children.isEmpty match { 
+              case true  => "had no dependencies"
+              case false => s"dependencies' details: ${children.map(child =>
+                           s"\ndependency ${child.data} - ${doSerialize(child)}")}"
+            }
       }"
     }
     
