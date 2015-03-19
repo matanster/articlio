c1f78aca02eea06d70971cf313c69d87b4fe3dbb
matan <dev.matan@gmail.com>
2015-03-19 02:19:54 +0200

    "cleanup"

--- src/com/articlio/spray/httpService.scala
+++ src/com/articlio/spray/httpService.scala
@@ -1,31 +1,16 @@
 package com.articlio.spray
-/*
- * 
- * This spray code is now dead code - been migrated to the play routes.. to be deleted soon 
- * 
- */
+import com.articlio.pipelines.concrete._
 
-/*
-
-package com.articlio.semantic
-import com.articlio.input.JATS
-import com.articlio.util.runID
-import com.articlio.config
-import com.articlio.ldb
-
-//
-// Spray imports
-//
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
 import spray.can.Http
-import akka.pattern.ask
 import akka.util.Timeout
 import akka.actor.Actor
 import spray.routing._
 import spray.http._
-import MediaTypes._
+import spray.http.MediaTypes._
 import scala.concurrent.duration._
+import spray.routing.Directive.pimpApply
 
 class MyServiceActor extends Actor with MyService {
 
@@ -41,50 +26,29 @@ class MyServiceActor extends Actor with MyService {
 
 // this trait defines our service behavior independently from the service actor
 trait MyService extends HttpService {
-
+  
   val myRoute =
     get {
 	  path("") { 
-		  complete {
-        "${buildVersioning.Info.name} service is up"        }
-      } ~
-      path("semantic") {
-        parameter('inputFile) { inputFile =>
-      	  ldb.ldb.go("SingleFileRun" + "-" + (new runID).id, new JATS(s"${config.pdf}/$inputFile", "pdf-converted"))
-      	  complete("Done processing file")
-        } ~
-        parameter('eLifeInputFile) { eLifeInputFile =>
-      	  ldb.ldb.go("SingleFileRun" + "-" + (new runID).id, new JATS(s"${config.eLife}/$eLifeInputFile"))
-          complete("Done processing file")
-        } ~       
-        parameter('all) { all =>
-          val bulk = new Bulk((new runID).id)
-          bulk.all
-          complete("Done processing all files... but you probably timed out by now")
-        } ~
-        parameter('allp) { all =>
-          val bulk = new Bulk((new runID).id)
-          bulk.allPDF
-          complete("Done processing all pdf files... but you probably timed out by now")
-        } ~
-        parameter('alle) { all =>
-          val bulk = new Bulk((new runID).id)
-          bulk.alleLife
-          complete("Done processing all eLife files... but you probably timed out by now")
-        } ~       
-        parameter('export) { all =>
-          //com.articlio.storage.createAnalyticSummary.go()
-          com.articlio.storage.createCSV.go()
-          complete("Done producing result CSVs")
-        } ~    
-          parameter('purgeAll) { purgeAll => 
-          AppActorSystem.outDB ! "dropCreate"
-          complete("purging all semantic data...")
+  		complete {
+          //<html><body><h1>{buildVersioning.Info.name} service is up</h1></body></html>
+          "${buildVersioning.Info.name} service is up"
         }
+    } ~
+    path("filesTransformer") {
+      parameter('all) { _ =>
+        val eLifeJATSpipeline = new JATSpipeline
+        val PDFpipeline = new ConvertedCorpusPipeline
+    	  complete("Done transforming input files")
+      } ~
+      parameter('inputFile) { inputFile =>
+        complete("Processing single input file not implemented yet")
       }
     }
+  }
 }
 
+/*
 object HttpService {
 
   // ActorSystem for spray
@@ -96,8 +60,7 @@ object HttpService {
   implicit val timeout = Timeout(6000.seconds)
   
   // start http listener
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 3001)
+  IO(Http) ? Http.Bind(service, interface = "localhost", port = 3333)
  
 }
-
 */
