fbe54a353d5817825594a561dc5e335dc6c489b1
matan <dev.matan@gmail.com>
2015-04-26 09:35:53 +0300

    "logging re-implementation: output modifiers per message type (normal, error)"

--- app/src/com/articlio/logger/logger.scala
+++ app/src/com/articlio/logger/logger.scala
@@ -6,6 +6,10 @@ abstract class logTag
 object Performance extends logTag
 object Default     extends logTag
 
+abstract class MessageType
+object Normal extends MessageType
+object Error extends MessageType
+
 abstract class Console
 object ConsoleMirror extends Console
 
@@ -26,39 +30,50 @@ abstract class inclusiveTagFilter(includeTags: Seq[logTag]) extends TagFilter {
 } 
 
 abstract class Expander {
-  def apply(msg: String, msgTags: Seq[logTag]) : String
+  def apply(msg: String, msgTags: Seq[logTag], messageType: MessageType) : String
 }
 
 object DefaultExpander extends Expander {
-  def apply(msg: String, tags: Seq[logTag]) : String = {
-    tags.map(tag => "(" + tag.getClass.getSimpleName.dropRight(1) + ")").mkString(" ") + " " + msg
+  def apply(msg: String, tags: Seq[logTag], messageType: MessageType) : String = {
+    val result = tags.map(tag => "(" + tag.getClass.getSimpleName.dropRight(1) + ")").mkString(" ") + " " + msg
+    messageType match {
+      case Normal => result 
+      case Error  => "Error: " + result
+    }
   }
 }
 
 abstract class UnderlyingExternalLogger {
-  def apply(finalMessage: String) 
+  def apply(finalMessage: String, messageType: MessageType) 
 }
 
 object DefaultUnderlyingExternalLogger extends UnderlyingExternalLogger {
   //import org.slf4j._
   //import org.slf4j.LoggerFactory._
-  val externalLogger = {
-    org.slf4j.LoggerFactory.getLogger("articlio")
-  }
+  val externalLogger = org.slf4j.LoggerFactory.getLogger("articlio")
 
-  def apply(finalMessage: String) = {
-    externalLogger.info(finalMessage)
+  def apply(finalMessage: String, messageType: MessageType) = {
+    import Console._
+    messageType match {
+      case Normal => externalLogger.info(finalMessage)
+      case Error  => externalLogger.error(RED_B + WHITE + finalMessage + RESET)
+    }
   }
 } 
 
-case class Logger(tagFilter: TagFilter, expander: Expander, underlyingExternalLogger: UnderlyingExternalLogger) {
+case class Logger(tagFilter: TagFilter, 
+                  expander: Expander, 
+                  underlyingExternalLogger: UnderlyingExternalLogger) {
   
-  def log(msg: String, tags: Seq[logTag] = Seq(Default), console: Option[Console] = None) = {
+  def log(msg:         String, 
+          tags:        Seq[logTag]     = Seq(Default), 
+          console:     Option[Console] = None, 
+          messageType: MessageType     = Normal) = {
 
     val commonTags = tagFilter(tags)
     commonTags.nonEmpty match {
       case true =>
-        DefaultUnderlyingExternalLogger(expander(msg, commonTags))
+        DefaultUnderlyingExternalLogger(expander(msg, commonTags, messageType), messageType)
         console match {
           case Some(ConsoleMirror) => Console.print(msg)
           case Some(_) =>
